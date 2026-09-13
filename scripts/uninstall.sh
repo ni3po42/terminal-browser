70b014bbee31f7a73aa1bd6a3c1c8190fd11aa52
Tim Stratton <ni3po42@gmail.com>
2026-09-13 12:33:46 -0500

    Create uninstall.sh
    added unintall script

---
+++ scripts/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -fr  "~/.agents/skills/terminal-browser",
+rm -fr  "~/.cache/terminal-browser-*",
+rm -fr  "~/.claude/skills/terminal-browser",
+rm -fr  "~/.codex/skills/terminal-browser",
+rm -fr  "~/.cursor/skills/terminal-browser",
+rm -fr  "~/.gemini/skills/terminal-browser",
+rm -fr  "~/.local/share/terminal-browser-*",
+rm -fr  "~/.local/state/terminal-browser",
+rm -fr  "~/.local/state/terminal-browser-*",
+rm -fr  "~/.local/bin/terminal-browser"
