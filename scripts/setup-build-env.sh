8f9c74ebca14ee0f38ef394b3389992bf55b075e
Tim Stratton <ni3po42@gmail.com>
2026-09-13 14:10:43 -0500

    Update setup-build-env.sh

--- scripts/setup-build-env.sh
+++ scripts/setup-build-env.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # cargo
 curl https://sh.rustup.rs -sSf | sh
+source ~/.bashrc
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash
+source ~/.bashrc
+
 # npm
 echo node > .nvmrc
 nvm install
+source ~/.bashrc
 
 # esbuild
 npm install --save-exact --save-dev esbuild
